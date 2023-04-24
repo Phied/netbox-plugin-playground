67d9c8e7a77b6f606e14229087e59312de0d195f
Keith Perkins <keith@perkins.land>
2023-04-24 15:27:19 -0500

    dadf

--- netbox_bgp_playground/__init__.py
+++ netbox_bgp_playground/__init__.py
@@ -1,10 +1,10 @@
 from extras.plugins import PluginConfig
 
-class NetboxBgpPlaygroundConfig(PluginConfig):
+class NetboxBgpPlayground(PluginConfig):
     name =  'netbox-bgp-playground'
     verbose_name = 'Netbox BGP Playground'
     description = 'Manage BGP Stuffs'
     version =  '0.01'
     base_url = 'bgp-playground'
 
-config = NetboxBgpPlaygroundConfig
+config = NetboxBgpPlayground
