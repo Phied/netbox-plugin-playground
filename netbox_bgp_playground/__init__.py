18cb316db3c274bf746272417ce664b124cfcdca
Keith Perkins <keith@perkins.land>
2023-04-24 15:10:47 -0500

    Renamed again

--- netbox_bgp_playground/__init__.py
+++ netbox_bgp_playground/__init__.py
@@ -1,10 +1,10 @@
 from extras.plugins import PluginConfig
 
-class NetboxPluginPlaygroundConfig(PluginConfig):
-    name =  'netbox_plugin_playground'
+class NetboxBgpPlaygroundConfig(PluginConfig):
+    name =  'netbox_bgp_playground'
     verbose_name = 'Netbox BGP Playground'
     description = 'Manage BGP Stuffs'
     version =  '0.01'
     base_url = 'netbox-plugin-playground'
 
-config = NetboxPluginPlaygroundConfig
+config = NetboxBgpPlaygroundConfig
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox-plugin-playground',
+    name='netbox-bgp-playground',
     version='0.01',
     description='A BGP Playground Plugin',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
 )
