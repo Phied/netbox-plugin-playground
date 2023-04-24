9eb361d86ae3f8d8704c1b94703dd81a1145b95b
Keith Perkins <keith@perkins.land>
2023-04-24 15:20:01 -0500

    remove "netbox" from url

--- netbox_bgp_playground/__init__.py
+++ netbox_bgp_playground/__init__.py
@@ -1,10 +1,10 @@
 from extras.plugins import PluginConfig
 
 class NetboxBgpPlaygroundConfig(PluginConfig):
-    name =  'netbox-bgp-playground'
+    name =  'netbox_bgp_playground'
     verbose_name = 'Netbox BGP Playground'
     description = 'Manage BGP Stuffs'
     version =  '0.01'
-    base_url = 'netbox-bgp-playground'
+    base_url = 'bgp-playground'
 
 config = NetboxBgpPlaygroundConfig
