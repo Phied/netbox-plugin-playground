c691a490b5772bddc1d7d8708e65b92d203ee383
Keith Perkins <keith@perkins.land>
2023-04-24 16:33:37 -0500

    Added a model

---
+++ netbox_bgp/models.py
@@ -0,0 +1,22 @@
+from netbox.models import NetBoxModel
+from django.db import models
+from utilities.choices import ChoiceSet
+
+
+class ActionChoices(ChoiceSet):
+    key = 'BgpCommunity.status'
+
+    CHOICES = [
+        ('enabled', 'Enabled', 'green'),
+        ('disabled', 'Disabled', 'red'),
+        ('reserved', 'Reserved', 'yellow')
+    ]
+
+class BgpCommunity(NetBoxModel):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=30, choices=ActionChoices, null=False, blank=False)
+    tenant = models.CharField(max_length=30, on_delete=models.PROTECT, to='tenancy.Tenant', related_name='+', blank=True, null=True)
+
+    def get_status_color(self):
+        return ActionChoices.colors.get(self.status)
