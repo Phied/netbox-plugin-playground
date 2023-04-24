77e2e7551c4eba4fda6cf304ab8cd0fb0b6f36fb
Keith Perkins <keith@perkins.land>
2023-04-24 15:36:18 -0500

    Test with accesss-lists

--- netbox_bgp_playground/__init__.py
+++ netbox_bgp_playground/__init__.py
@@ -1,10 +1,12 @@
 from extras.plugins import PluginConfig
 
-class NetboxBgpPlaygroundConfig(PluginConfig):
-    name =  'bgp_playground'
-    verbose_name = 'Netbox BGP Playground'
-    description = 'Manage BGP Stuffs'
-    version =  '0.01'
-    base_url = 'bgp_playground'
 
-config = NetboxBgpPlaygroundConfig
+class NetBoxAccessListsConfig(PluginConfig):
+    name = 'netbox_access_lists'
+    verbose_name = ' NetBox Access Lists'
+    description = 'Manage simple ACLs in NetBox'
+    version = '0.1'
+    base_url = 'access-lists'
+
+
+config = NetBoxAccessListsConfig
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox-bgp-playground',
-    version='0.01',
-    description='A BGP Playground Plugin',
+    name='netbox-access-lists',
+    version='0.1',
+    description='An example NetBox plugin',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
 )
