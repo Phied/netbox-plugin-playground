8a173a3c804b900b82367427c49d89ea897b2eb0
Keith Perkins <keith@perkins.land>
2023-04-25 10:41:54 -0500

    Created first migrations

--- netbox_bgp/models.py
+++ netbox_bgp/models.py
@@ -17,7 +17,7 @@ class BgpCommunity(NetBoxModel):
     name = models.CharField(max_length=100, blank=False)
     description = models.CharField(max_length=100, blank=True, null=True)
     status = models.CharField(max_length=30, choices=ActionChoices, null=False, blank=False)
-    tenant = models.CharField(max_length=30, on_delete=models.PROTECT, to='tenancy.Tenant', related_name='+', blank=True, null=True)
+    tenant = models.ForeignKey(to='tenancy.Tenant', on_delete=models.PROTECT, related_name='+', blank=True, null=True)
 
 class Meta:
     ordering = ('name', 'index')
