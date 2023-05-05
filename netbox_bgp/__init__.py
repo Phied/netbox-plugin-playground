041715ede01cdca2144930887cd0073ec4588989
Keith Perkins <keith@perkins.land>
2023-05-05 11:00:38 -0500

    Added Release Version

--- netbox_bgp/__init__.py
+++ netbox_bgp/__init__.py
@@ -3,10 +3,10 @@ from extras.plugins import PluginConfig
 class BgpConfig(PluginConfig):
     name =  'netbox_bgp'
     verbose_name = 'Netbox BGP'
-    description = 'Beta to manage Netbox BGP Attributes'
+    description = 'A way to manage Netbox BGP Attributes'
     author = 'Keith Perkins'
     author_email = 'keithaperkins@gmail.com'
-    version =  '0.01'
+    version =  'v0.0.1beta'
     base_url = 'bgp'
 
 config = BgpConfig
