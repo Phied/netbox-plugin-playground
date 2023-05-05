63966b1e31d56bd4e0b459d69598f2eaa941d6f7
Keith Perkins <keith@perkins.land>
2023-05-05 11:03:42 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-bgp',
-    version='0.01',
-    description='A BGP Playground Plugin',
+    version='v0.0.1beta',
+    description='A way to manage Netbox BGP Attributes',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
